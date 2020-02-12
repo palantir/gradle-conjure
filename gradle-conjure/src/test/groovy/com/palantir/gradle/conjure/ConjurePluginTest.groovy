e1e4b05dd0bcf51addc7f1ea546abdc1ce4d3499
Robert Kruszewski <robert@robert.sx>
2020-02-12 13:52:34 +0000

    Fix: Use jakarta-api instead javax-api (#371)
    Stop using legacy javax.ws.rs:javax.ws.rs-api when compiling jersey projects

--- gradle-conjure/src/test/groovy/com/palantir/gradle/conjure/ConjurePluginTest.groovy
+++ gradle-conjure/src/test/groovy/com/palantir/gradle/conjure/ConjurePluginTest.groovy
@@ -78,7 +78,7 @@ class ConjurePluginTest extends IntegrationSpec {
         com.palantir.conjure.java:* = ${TestVersions.CONJURE_JAVA}
         com.palantir.conjure:conjure = ${TestVersions.CONJURE}
         com.squareup.retrofit2:retrofit = 2.1.0
-        javax.ws.rs:javax.ws.rs-api = 2.0.1
+        jakarta.ws.rs:jakarta.ws.rs-api = 2.1.6
         """.stripIndent()
 
         createFile('api/src/main/conjure/api.yml') << '''
