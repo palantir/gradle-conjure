78ac1fd6876bb9950c998e8c9175570bf834776f
forozco <forozco@palantir.com>
2020-08-24 15:05:19 -0400

    bump

--- gradle-conjure/src/test/groovy/com/palantir/gradle/conjure/TestVersions.java
+++ gradle-conjure/src/test/groovy/com/palantir/gradle/conjure/TestVersions.java
@@ -19,7 +19,7 @@ package com.palantir.gradle.conjure;
 public final class TestVersions {
     private TestVersions() {}
 
-    public static final String CONJURE = "4.11.1";
+    public static final String CONJURE = "4.11.2";
     public static final String CONJURE_JAVA = "5.17.0";
     public static final String CONJURE_PYTHON = "3.11.6";
     public static final String CONJURE_TYPESCRIPT = "3.8.1";
