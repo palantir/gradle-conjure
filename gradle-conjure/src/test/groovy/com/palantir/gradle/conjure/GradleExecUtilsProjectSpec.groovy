a7292fd8b96686c26463c6ab45c20c741549d41e
Jess Ochs-Willard <jessow@palantir.com>
2022-02-22 18:06:46 -0500

    mock exec ops

--- gradle-conjure/src/test/groovy/com/palantir/gradle/conjure/GradleExecUtilsProjectSpec.groovy
+++ gradle-conjure/src/test/groovy/com/palantir/gradle/conjure/GradleExecUtilsProjectSpec.groovy
@@ -18,11 +18,18 @@ package com.palantir.gradle.conjure
 
 import nebula.test.ProjectSpec
 import org.assertj.core.api.Assertions
+import org.gradle.api.Action
+import org.gradle.api.Project
+import org.gradle.process.ExecOperations
+import org.gradle.process.ExecResult
+import org.gradle.process.ExecSpec
+import org.gradle.process.JavaExecSpec
 
 class GradleExecUtilsProjectSpec extends ProjectSpec {
     def 'running a program that exits with code 0 does not throw an exception'() {
         expect:
-        GradleExecUtils.exec(project, 'execute', new File('/bin/sh'), ['-c'], ['exit 0'])
+        ExecOperations mockExecOperations = new MockExecOperations(project)
+        GradleExecUtils.exec(mockExecOperations, 'execute', new File('/bin/sh'), ['-c'], ['exit 0'])
     }
 
     def 'running a program that exits with a non-zero code throws an exception containing both stdout and stderr'() {
@@ -30,12 +37,30 @@ class GradleExecUtilsProjectSpec extends ProjectSpec {
         def baseArgs = ['-c']
         def extraArgs = ['echo foo; echo bar >&2; exit 1']
 
+        ExecOperations mockExecOperations = new MockExecOperations(project)
         Assertions.assertThatExceptionOfType(RuntimeException).isThrownBy {
-            GradleExecUtils.exec(project, 'fail', new File('/bin/sh'), baseArgs, extraArgs)
+            GradleExecUtils.exec(mockExecOperations, 'fail', new File('/bin/sh'), baseArgs, extraArgs)
         }.withMessageContaining("Failed to fail.")
                 .withMessageContaining((baseArgs + extraArgs).join(", "))
                 .withMessageContaining("failed with exit code 1. Output:")
                 .withMessageContaining("foo\n")
                 .withMessageContaining("bar\n")
+    }
+
+    class MockExecOperations implements ExecOperations {
+        Project project
+
+        MockExecOperations(Project project) {
+            this.project = project
+        }
+        @Override
+        ExecResult exec(Action<? super ExecSpec> spec) {
+            return project.exec(spec)
+        }
+
+        @Override
+        ExecResult javaexec(Action<? super JavaExecSpec> spec) {
+            return project.javaexec(spec)
+        }
     }
 }
