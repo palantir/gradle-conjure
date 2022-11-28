60599a689d2f02f846bc7a0ad42e1f47415795bb
Carter Kozak <ckozak@palantir.com>
2022-11-28 16:05:27 -0500

    Fix exact dependency check failures in conjure-java-local

--- gradle-conjure/src/main/java/com/palantir/gradle/conjure/ConjurePlugin.java
+++ gradle-conjure/src/main/java/com/palantir/gradle/conjure/ConjurePlugin.java
@@ -295,7 +295,7 @@ public final class ConjurePlugin implements Plugin<Project> {
     }
 
     @SuppressWarnings({"unchecked", "RawTypes"})
-    private static void ignoreFromCheckUnusedDependencies(Project proj) {
+    static void ignoreFromCheckUnusedDependencies(Project proj) {
         proj.getPlugins().withId("com.palantir.baseline-exact-dependencies", plugin -> {
             Class<? extends Task> checkUnusedDependenciesTask;
             try {
