6d0da0ce7655a6093730baa3880909dbe20e9c1d
Arttu Voutilainen <avoutilainen@palantir.com>
2024-04-10 12:29:38 +0200

    correctly parse product name out of filename also when product version is something else than "1.2.3", ie. RCs and commits

--- gradle-conjure/src/main/java/com/palantir/gradle/conjure/ConjureLocalGenerateTask.java
+++ gradle-conjure/src/main/java/com/palantir/gradle/conjure/ConjureLocalGenerateTask.java
@@ -16,17 +16,41 @@
 
 package com.palantir.gradle.conjure;
 
+import com.palantir.sls.versions.OrderableSlsVersion;
+import groovy.lang.Tuple;
+import groovy.lang.Tuple2;
 import java.io.File;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.gradle.api.tasks.CacheableTask;
 
 @CacheableTask
 public abstract class ConjureLocalGenerateTask extends ConjureGeneratorTask {
 
+    protected static final Pattern PATTERN = Pattern.compile("^([^.]+)-(.+?)(\\.conjure)?\\.json$");
+
+    static Tuple2<String, OrderableSlsVersion> parseProductNameAndVersion(String filename) {
+        Matcher matcher = PATTERN.matcher(filename);
+        if (!matcher.matches() || matcher.groupCount() < 2) {
+            throw new RuntimeException(String.format("Unable to parse conjure dependency name %s", filename));
+        }
+        String irName = matcher.group(1);
+        Optional<OrderableSlsVersion> maybeIrVersion = OrderableSlsVersion.safeValueOf(matcher.group(2));
+
+        if (maybeIrVersion.isEmpty()) {
+            throw new RuntimeException(String.format(
+                    "Unable to parse orderable SLS version from conjure dependency name %s, version %s",
+                    filename, maybeIrVersion));
+        }
+        return Tuple.tuple(irName, maybeIrVersion.get());
+    }
+
     @Override
     protected final File outputDirectoryFor(File file) {
         // Strip extension and version
         return getOutputDirectory()
-                .dir(file.getName().substring(0, file.getName().lastIndexOf("-")))
+                .dir(parseProductNameAndVersion(file.getName()).getV1())
                 .get()
                 .getAsFile();
     }
